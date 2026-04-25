754822d2095cc4192b8667524a18d603d4ae5efb
landim9 <landimr9des@gmail.com>
2026-04-25 16:00:32 -0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { verificarSessao } from '@/lib/sessions'
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('sessao')?.value
 
-    const token = request.cookies.get('sessao')?.value
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
-    if (!token) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
-    return NextResponse.next()
+  const usuarioId = await verificarSessao(token)
+
+  if (!usuarioId) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'] // aplica só nas rotas do dashboard
+  matcher: ['/dashboard/:path*']
 }
